746f94fda687e1d7c98c0f120a61b2e8f96c8eae
joerho1@gmail.com <joerho1@gmail.com>
2020-08-18 13:59:43 -0700

    add animation

--- sketch.js
+++ sketch.js
@@ -1,8 +1,29 @@
 function setup() {
 	createCanvas(600, 600);
-	background(0)
+	background(255);
+	
 }
 
-function draw() {
+var x = 300;
+var y = 300;
+var animate = true;
 
+function draw() {
+	background(255);
+	fill(255,0,0);
+	ellipse(x,y, 50,50);
+
+	if (animate == true) {
+		y += 5;
+	}
+	else {
+		y -= 5;
+	}
+
+	if (y > 550) {
+		animate = false;
+	}
+	if (y < 50) {
+		animate = true;
+	}
 }
